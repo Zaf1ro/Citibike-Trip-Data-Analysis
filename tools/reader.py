f74c457f38d35e0936b60cff0829efd3eda09e85
Jason <duan_jason@outlook.com>
2018-07-29 13:31:26 -0400

    fix bugs in reader

--- tools/reader.py
+++ tools/reader.py
@@ -1,11 +1,7 @@
 # !/usr/bin/python
 # coding: utf-8
 
-
-import numpy as np
 import pandas as pd
-import time, datetime
-import matplotlib.pyplot as plt
 
 
 # Read Citibike data from csv file in one mouth
@@ -13,8 +9,10 @@ def read_data_one_mouth(csv_file_s):
     return pd.DataFrame(pd.read_csv(csv_file_s))
 
 
+# Read csv file by specific date
 def read_data(year, start_month, end_month):
     data = None
+
     while start_month <= end_month:
         csv_filename = '%d%02d%s' % (year, start_month, '-citibike-tripdata.csv')
         month_data = read_data_one_mouth('data/' + csv_filename)
